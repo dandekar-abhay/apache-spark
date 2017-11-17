8d99c5c555e4ff40b52ef4f3ab3b34dc68076701
Abhay Dandekar <dandekar.abhay@gmail.com>
2017-11-17 14:09:25 +0530

    Controller updated

--- spark.kafka.poc/src/main/java/apache/spark/poc/controller/Controller.java
+++ spark.kafka.poc/src/main/java/apache/spark/poc/controller/Controller.java
@@ -10,75 +10,81 @@ import org.apache.spark.launcher.SparkLauncher;
 
 public class Controller {
 
-	class StatusDisplayer extends TimerTask {
+  class StatusDisplayer extends TimerTask {
 
-		SparkAppHandle handle;
+    SparkAppHandle handle;
 
-		@Override
-		public void run() {
-			System.out.println("Spark Status: " + handle.getState());
-		}
+    @Override
+    public void run() {
+      System.out.println("Spark Status: " + handle.getState());
+    }
 
-		public StatusDisplayer(SparkAppHandle handle) {
-			this.handle = handle;
-		}
+    public StatusDisplayer(SparkAppHandle handle) {
+      this.handle = handle;
+    }
 
-	}
+  }
 
-	class SparkStopper extends TimerTask {
+  class SparkStopper extends TimerTask {
 
-		SparkAppHandle handle;
-		
-		boolean alreadyExecuted = false;
+    SparkAppHandle handle;
 
-		@Override
-		public void run() {
-			
-			if ( !alreadyExecuted) {
-				File spinFile = new File("/tmp/stop.spark");
-				if (spinFile.exists()) {
-					
-					alreadyExecuted = true;
-					System.out.println("Found spinfile ... Stopping spark");
-					handle.stop();
-					// handle.kill();
-					System.out.println("Handle stop executed...");
-				}
-			}
-			
-		}
+    boolean alreadyExecuted = false;
 
-		public SparkStopper(SparkAppHandle handle) {
-			this.handle = handle;
-		}
+    @Override
+    public void run() {
 
-	}
+      if (!alreadyExecuted) {
+        File spinFile = new File("/tmp/stop.spark");
+        if (spinFile.exists()) {
 
-	private void execute() {
-		SparkLauncher launcher = new SparkLauncher().setAppResource(
-				"/home/abhay/MyHome/WorkHome/CodeHome/Apache/Spark/MyExamples/Java/apache-spark/spark.kafka.poc/target/kafka.poc-0.0.3-SNAPSHOT.jar")
-				.setMainClass("apache.spark.poc.SparkStructuredStreamProcessor").setMaster("local[2]")
-				.setConf(SparkLauncher.DRIVER_MEMORY, "1g").setConf(SparkLauncher.EXECUTOR_MEMORY, "1g")
-				.setConf(SparkLauncher.EXECUTOR_CORES, "1");
+          alreadyExecuted = true;
+          System.out.println("Found spinfile ... Stopping spark");
+          // handle.stop();
+          handle.kill();
+          System.out.println("Handle stop executed...");
+        }
+      }
 
-		try {
-			SparkAppHandle handle = launcher.startApplication();
-			Timer timer = new Timer();
-			TimerTask statusTask = this.new StatusDisplayer(handle);
-			TimerTask stopperTask = this.new SparkStopper(handle);
-			timer.schedule(statusTask, 1000, 3 * 1000);
-			timer.schedule(stopperTask, 1000, 5 * 1000);
+    }
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    public SparkStopper(SparkAppHandle handle) {
+      this.handle = handle;
+    }
 
-	}
+  }
 
-	public static void main(String[] args) {
+  // Enter your jar path location here
+  private final String jarPath =
+      "/home/abhay/MyHome/WorkArea/CodeHome/Apache/Spark/MyCodeEgs/Java/apache-spark/spark.kafka.poc/target/kafka.poc-0.0.3-SNAPSHOT.jar";
 
-		new Controller().execute();
+  private void execute() {
+    SparkLauncher launcher = new SparkLauncher().setAppResource(jarPath)
+        .setMainClass("apache.spark.poc.SparkStructuredStreamProcessor")
+        .setMaster("local[2]").setConf(SparkLauncher.DRIVER_MEMORY, "1g")
+        .setConf(SparkLauncher.EXECUTOR_MEMORY, "1g")
+        .setConf(SparkLauncher.EXECUTOR_CORES, "1");
 
-	}
+    try {
+      SparkAppHandle handle = launcher.startApplication();
+      Timer timer = new Timer();
+
+      TimerTask statusTask = this.new StatusDisplayer(handle);
+      TimerTask stopperTask = this.new SparkStopper(handle);
+
+      timer.schedule(statusTask, 1000, 3 * 1000);
+      timer.schedule(stopperTask, 1000, 5 * 1000);
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+  }
+
+  public static void main(String[] args) {
+
+    new Controller().execute();
+
+  }
 
 }
